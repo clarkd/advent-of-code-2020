04d0f51d52a1a784c992bf4cf5cf25b8fefda63d
Dave Clarke <dave.clarke@squaredup.com>
2020-12-06 17:45:12 +0000

    Day 2

---
+++ day-2/index.js
@@ -0,0 +1,31 @@
+const input = require('./input');
+const numbers = input.trim().split('\n');
+
+const part1 = () => {
+    console.log("=== Day 2: Part 2 ===");
+    const valid = numbers.filter(p => {
+        const [policy, password] = p.split(': ');
+        const [counts, letter] = policy.split(' ');
+        const [min, max] = counts.split('-').map(n => parseInt(n));
+        const count = (password.match(new RegExp(`${letter}`, 'g')) || []).length;
+        return count >= min && count <= max;
+    });
+
+    console.log(valid.length);
+};
+
+const part2 = () => {
+    console.log("=== Day 2: Part 2 ===");
+    const valid = numbers.filter(p => {
+        const [policy, password] = p.split(': ');
+        const [counts, letter] = policy.split(' ');
+        const [pos1, pos2] = counts.split('-').map(n => parseInt(n));
+        return password[pos1-1] === letter && password[pos2-1] !== letter
+         || password[pos1-1] !== letter && password[pos2-1] == letter;
+    });
+
+    console.log(valid.length);
+}
+
+part1();
+part2();
