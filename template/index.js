c6940c6e84b098722c6679e9f4ca6241c66ba3dd
Dave Clarke <dave.clarke@squaredup.com>
2020-12-10 00:16:08 +0000

    Add template

---
+++ template/index.js
@@ -0,0 +1,13 @@
+const input = require('./input');
+const data = input.trim().split('\n');
+
+const part1 = () => {
+    console.log("=== Day 6: Part 1 ===");  
+};
+
+const part2 = () => {
+    console.log("=== Day 6: Part 2 ===");   
+}
+
+part1();
+//part2();
